ba940e2e311cbc456ab6e0941e1df2ade48de3d0
Sirisha Veeralapalli <Sirisha.V@WinWire.com>
2023-02-21 13:49:59 +0530

    initial commit of assignment

---
+++ XamarinAssignment/App1/App1/App1/Home.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class Home : ContentPage
+    {
+        public Home()
+        {
+            InitializeComponent();
+            //Content = new Label
+            //{
+            //    Text = "Hello world fromm code behind..",
+            //    HorizontalOptions = LayoutOptions.Center,
+            //    VerticalOptions = LayoutOptions.Center
+            //};
+        }
+
+        //private void Slider1_ValueChanged(object sender, ValueChangedEventArgs e)
+        //{
+        //    label1.Text = e.NewValue.ToString();
+        //}
+    }
+}
+
---
+++ XamarinAssignment/App1/App1/App1/QuotesPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class QuotesPage : ContentPage
+    {
+        string sliderValue = "16";
+        public QuotesPage()
+        {
+            InitializeComponent();
+            sliderValue = slider1.Value.ToString() ;
+            fontSize.Text = "Font Size - " + sliderValue;
+            quote.Text = "You can't blame gravity for falling in love";
+            quote.FontSize = Convert.ToDouble(sliderValue);
+        }
+
+        void slider1_ValueChanged(System.Object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            sliderValue = slider1.Value.ToString(); ;
+            fontSize.Text = "Font Size - " + sliderValue;
+            quote.FontSize = Convert.ToDouble(sliderValue);
+        }
+    }
+}
+
